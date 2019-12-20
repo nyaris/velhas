e018b647a957476336cb7e1d9df85e39e5442a5d
Nina Dominique Thomé Bernardo - NBE <nina.bernardo@iteris.local>
2019-12-20 18:04:14 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { TabuleiroComponent } from './tabuleiro/tabuleiro.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabuleiroComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    SweetAlert2Module
   ],
   providers: [],
   bootstrap: [AppComponent]
